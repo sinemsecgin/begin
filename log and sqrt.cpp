a4514e0371b759cc8333076b51e2863f7ee8c5ad
Shinemu <secginsinem@gmail.com>
2014-06-19 02:41:45 +0300

    log,sqrtt and exponential

--- log and sqrt.cpp
+++ log and sqrt.cpp
@@ -8,19 +8,26 @@ Logarithm(){Lo=0;}
 Logarithm(double L){Lo=L;}
 double GetLogFunc(){ return log(Lo); }
 };
+
 class SquareRoot{
 public : double sqrtt;
 SquareRoot(){sqrtt=0;}
 SquareRoot(double sqr){sqrtt=sqr;}
 double GetSquareRoot(){return sqrt(sqrtt);}
 };
+class Exponential{
+public:
+double ex;
+Exponential()   {ex=0;}
+Exponential(double myexp)   {ex=myexp;}
+double GetExponential()  {return exp(ex);}
+};
 int main()
 {   double choice;
-    cout<<"Please Enter a Choice ( to find Logarthim = 1 OR SquareRoot = 2)";
+    cout<<"Please Enter a Choice ( to find Logarthim = 1 OR SquareRoot = 2 OR Exponential=3)";
     cin>>choice;
     if(choice==1)
 {
-
   while(1){
     double a;
     cout<<"Please enter a value : ";
@@ -38,5 +45,19 @@ while(1){
     cin>>b;
     SquareRoot value(b);
     cout<<" Square Root Equal to : "<<value.GetSquareRoot()<<endl;
+      if(b==0)
+  break;
     }
-return 0;}}
+}
+    if(choice==3){
+    while(1){
+        double c;
+    cout<<"Please Enter a Number : ";
+    cin>>c;
+    Exponential value(c);
+     cout<<" Exponential Number Equal to : "<<value.GetExponential()<<endl;
+      if(c==0)
+  break;}
+return 0;
+}
+}
