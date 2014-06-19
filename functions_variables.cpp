946fddf207f089ed1afa1cb45a557d6dfbf2fd7e
Kaan Genç <SeriousBug@Gmail.com>
2014-06-19 14:59:58 +0300

    Switched to an extensible inheritence system

--- functions_variables.cpp
+++ functions_variables.cpp
@@ -1,6 +1,16 @@
-class Variable {
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+class Var {
 public:
     char letter;
+    virtual double Value(double) = 0;
+};
+
+class Variable: public Var {
+public:
     double power;
     double coefficient;
     Variable(double coefficient=0, char letter=' ', double power=0) {
@@ -20,9 +30,9 @@ struct LVal{
 
 class Function {
 public:
-    Variable* variables;
+    Var* *variables;
     unsigned int arraySize;
-    Function(Variable variables[], unsigned int arraySize){
+    Function(Var* variables[], unsigned int arraySize){
         this->variables = variables;
         this->arraySize = arraySize;
     }
@@ -32,8 +42,10 @@ public:
         unsigned int val;
         for (var=0; var<arraySize; var++){
             for (val=0; val<valueSize; val++){
-                if (values[val].letter == variables[var].letter){
-                    value += variables[var].Value(values[val].value);
+                double tmp = values[val].value;
+
+                if (values[val].letter == variables[var]->letter){
+                    value += variables[var]->Value(values[val].value);
                 }
             }
         }
@@ -42,14 +54,14 @@ public:
 };
 
 int main(){
-    Variable testVar(4, 'x', 2 );  //4x^2
-    Variable doubleVar(-2.3, 'y', -0.5);    //(-2.3)x^(-0.5)
+    Var* testVar = new Variable(4, 'x', 2 );            //4x^2
+    Var* doubleVar = new Variable(-2.3, 'y', -0.5);    //(-2.3)x^(-0.5)
 
-    cout << "4x^2 at x = 3: " << testVar.Value(3) << endl
-         << "4x^2 at x = 5: " << testVar.Value(5) << endl
-         << "-2.3y^(-0.5) at y = 3.5: " << doubleVar.Value(3.5) << endl;
+    cout << "4x^2 at x = 3: " << testVar->Value(3) << endl
+         << "4x^2 at x = 5: " << testVar->Value(5) << endl
+         << "-2.3y^(-0.5) at y = 3.5: " << doubleVar->Value(3.5) << endl;
 
-    Variable vars[2] = {testVar, doubleVar};
+    Var* vars[2] = {testVar, doubleVar};
     Function func(vars, 2);
     LVal values[2] = {LVal{'x', 3}, LVal{'y', 3.5}};
     double result = func.Value(values, 2);
