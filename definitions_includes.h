7d15eebf1dd21a013181994bc8735fa82805d6b0
SeriousBug <kaan_genc@outlook.com>
2014-06-20 16:47:40 +0300

    Added license boilerplate.

--- definitions_includes.h
+++ definitions_includes.h
@@ -1,3 +1,21 @@
+/*
+    Begin: an all-purpose maths program.
+    Copyright (C) 2014 Sinem Seçgin
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef DEFINITIONS_INCLUDES_H_INCLUDED
 #define DEFINITIONS_INCLUDES_H_INCLUDED
 
@@ -7,5 +25,6 @@
 
 #define PI 3.141592653589793238462643383279502884197169399375105820974944
 #define E 2.718281828459045235360287471352662497757247093699959574966967
+#define CIRCLE 360
 
 #endif // DEFINITIONS_INCLUDES_H_INCLUDED
